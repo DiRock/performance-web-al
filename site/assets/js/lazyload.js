add626ec2fbb9d6caa61164533dfb9418bb76aa7
DiRock <araujo.die93@gmail.com>
2017-05-16 12:55:57 -0300

    Inserido técnica scroll throttle para aliviar o número de execuções do scripts enquanto se usa o scroll

--- site/assets/js/lazyload.js
+++ site/assets/js/lazyload.js
@@ -1,6 +1,18 @@
- window.onscroll = function () {
-        var imgs = document.querySelectorAll('img[data-src]:not([src])');
+var jarodei = false;
 
+    window.onscroll = function () {
+
+        // throttle
+        if (jarodei) return;
+        jarodei = true;
+        setTimeout(function () { 
+            jarodei = false; 
+        }, 200);
+
+        console.log('scroll');
+
+        // código em si
+        var imgs = document.querySelectorAll('img[data-src]:not([src])');
         for (var i = 0; i < imgs.length; i++) {
             if (imgs[i].getBoundingClientRect().top < window.innerHeight + 200) {
                 imgs[i].src = imgs[i].getAttribute('data-src');
